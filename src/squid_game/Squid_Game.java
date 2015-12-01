4210727c051ca52ed0e387f65a4a062ac2cc7801
Coopster108 <coopca108@gmail.com>
2015-11-30 21:35:56 -0800

    New commit

--- src/squid_game/Squid_Game.java
+++ src/squid_game/Squid_Game.java
@@ -22,24 +22,31 @@ public class Squid_Game {
         int lifeCount; //may or may not turn this into an incrementor method later
         lifeCount = 5;
         
+        
+        
         String color;
         Scanner kb = new Scanner(System.in);
         System.out.println("You are Squidward Tenticals stuck\n"
                 + "in the future. You've arrived in an\n"
                 + "empty room with virtual\n"
-                + "transportation squares.\n"
-                + "Choose a square\n"
+                + "transportation squares.");
+                
+        do{//restart for drops through the whole game
+                System.out.println("\nLives: "
+                + lifeCount
+                + "\nChoose a square\n"
                 + "\n"
                 + "[red, blue, green purple]");
         
     color = kb.next();
     
+    //********************************BLUE**************************************
+    
     if(color.charAt(0)=='b') //**BLUE** path
     {
-        System.out.println("Blue");
+        System.out.println("\nBlue");
         //blue branches below
-            System.out.println("You've been ejected into space. You're moving at dangerous speeds. Slow down!\n"
-                    + "\n");
+            System.out.println("You've been ejected into space. You're moving at dangerous speeds. Slow down!\n");
             
             //do while loop
             double speed;
@@ -96,7 +103,8 @@ public class Squid_Game {
                     ans = kb.next();
                     if(ans.charAt(0)== 'y')
                     {
-                        System.out.println("\n\nCongrats! You made it safely back to Bikini Bottom with: " + lifeCount + " lives!");
+                        System.out.println("\nCongrats! You made it safely back to Bikini Bottom with: " + lifeCount + " lives!");
+                        System.exit(0);//so it doesnt fall through to restart
                     }
                     else
                     {
@@ -127,10 +135,12 @@ public class Squid_Game {
 
     }
         
+    //***********************************GREEN***********************************
+    
     else //by ER
         if(color.charAt(0)=='g') //**GREEN** path
         {
-            System.out.println("Green");
+            System.out.println("\nGreen");
             //green branches below
             System.out.println("Oh no!\n"
                     + "You've gone even further into the future!" 
@@ -152,10 +162,11 @@ public class Squid_Game {
                     System.out.println("\nEnter a distress signal: ");
                     ans2 = kb.next();
                     System.out.println("\nYour distress signal was picked up by a freindly pirate!");
-                    System.out.println("\n\nCongrats! You made it safely back to Bikini Bottom with: " + lifeCount + " lives!");
+                    System.out.println("\nCongrats! You made it safely back to Bikini Bottom with: " + lifeCount + " lives!");
                     
+                    System.exit(0);//so it doesnt fall through to restart
                     }
-            } while(year < 2500 || year > 2600);
+            } while(year < 2500 || year > 2600 && lifeCount > 0);
             
             //next step
             //these steps require methods in separate classes, just do sout-tabs containing the info, I'll modify them later
@@ -164,10 +175,13 @@ public class Squid_Game {
             
             
         }
+        
+        //********************************PURPLE********************************
+        
         else    //by ER
             if(color.charAt(0)=='p') //**PURPLE** path
             {
-                System.out.println("Purple");
+                System.out.println("\nPurple");
                 //purple branches below
                System.out.println("You're trapped in a capsule at the\n"
                     + " bottom of the ocean.\n"
@@ -180,22 +194,123 @@ public class Squid_Game {
                    passcode = kb.nextInt();
                } while(passcode > 2); //for now just enter 1, must create method in *separate* class 
                
-                System.out.println("Now choose a direction [n/s/e/w] \n");
+               String dir;
+               do{ //next step
+                System.out.println("\nNow choose a direction [n/s/e/w]");
+                dir = kb.next();
                 
+                //by CTW
+                
+                String ans3;
+                if(dir.charAt(0) == 's') //SOUTH
+                {
+                    
+                    System.out.println("\nYou've been caught by a fishnet!");
+                    System.out.println("\nDo you try to escape?"); //next step
+                    ans3 = kb.next();
+                    if(ans3.charAt(0) == 'y')
+                    {
+                        System.out.println("\nYou're strangled & lose a life!");
+                        --lifeCount;
+                        System.out.println("Lives: " + lifeCount);
+                    }
+                    else
+                    {
+                        System.out.println("\nYou're turned into Sushi & lose a life!");
+                        --lifeCount;
+                        System.out.println("Lives: " + lifeCount);
+                    }
+                }
+                
+                if(dir.charAt(0) == 'w') //WEST
+                {
+                    do{
+                    System.out.println("\nWould you like to eat radio-active algae or braised plankton?");
+                    ans3 = kb.next();
+                    if(ans3.charAt(0)== 'b')
+                    {
+                        System.out.println("You are poisioned & lose a life!");
+                        --lifeCount;
+                        System.out.println("Lives: " + lifeCount);
+                    }
+                    
+                    else
+                    {
+                        System.out.println("\nYou gained a life & evoled into buff squid!");
+                        ++lifeCount;
+                        System.out.println("Lives: " + lifeCount);
+                        
+                        String ans;
+                        
+                        do
+                        {
+                        System.out.println("\nA submarine approaches you. Would you like to become a stowaway?");
+                        ans = kb.next();
+                        if(ans.charAt(0)== 'y')
+                        {
+                        System.out.println("\nCongrats! You made it safely back to Bikini Bottom with: " + lifeCount + " lives!");
+                        System.exit(0);//so it doesnt fall through to restart
+                        }
+                        
+                        else
+                        {
+                        System.out.println("You get lost in the dark ocean & lose a life!");
+                        --lifeCount;
+                        System.out.println("Lives: " + lifeCount);
+
+                        if(lifeCount == 0)//game over & 0 lives
+                            {
+                                System.out.println("--You died! Game Over--");
+                            }
+                        }
+                        
+                        }while(ans.charAt(0)== 'n'&& lifeCount > 0);
+                    }
+                    
+                    }while(ans3.charAt(0) != 'r' && lifeCount > 0);
+                    
+                    
+                }
+                
+                if(dir.charAt(0) == 'e')//EAST
+                {
+                    System.out.println("\nYou've been attacked by sharks & lose a life!");
+                    --lifeCount;
+                    System.out.println("Lives: " + lifeCount);
+                }
+                
+                if(dir.charAt(0) == 'n')//NORTH
+                {
+                    System.out.println("You've been attacked by zombie pinapples & lose a life!");
+                    --lifeCount;
+                    System.out.println("Lives: " + lifeCount);
+                }
+               }while(dir.charAt(0) != 'w' 
+                       && lifeCount > 0); //no n,e,s as it always loops to direction again
                 //next step 
                 //if else statements
                 //by CW
                 
                 
         }
-            else //by ER
+            
+     //********************************RED**************************************       
+            
+    else //by ER
             {
                 lifeCount = lifeCount - 1;
-                System.out.println("Red"); //**RED** path
+                System.out.println("\nRed"); //**RED** path
                 System.out.println("You've descended into a bottomless void. Lose 1 life.\n");
-                System.out.println("Go back to square one! \n");
-                System.out.println("Life Count: " + lifeCount);
+                System.out.println("Go back to square one!");
             }
+    
+    }while(lifeCount > 0);//added a restart for full drops through the game
+      
+        if(lifeCount == 0)
+        {
+            System.out.println("\nLives: " + lifeCount);
+            System.out.println("You lose!");
+        }
     }
     
 }
